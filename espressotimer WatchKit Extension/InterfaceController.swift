38e192a7c4fae7a99307774c18827380f32175b6
Hans-Wilhelm Warlo <hw@warlo.no>
2018-02-27 20:57:00 +0100

    Add microphone listener for watchos and ios

--- espressotimer WatchKit Extension/InterfaceController.swift
+++ espressotimer WatchKit Extension/InterfaceController.swift
@@ -7,11 +7,116 @@
 //
 
 import WatchKit
+import AVFoundation
 import Foundation
 
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet var button: WKInterfaceButton!
+    @IBOutlet var timerLabel: WKInterfaceLabel!
+    @IBOutlet var mic: WKInterfaceLabel!
+    
+    var recordingSession: AVAudioSession!
+    var audioRecorder: AVAudioRecorder!
+    var recorder : AVAudioRecorder? = nil
+    
+    var time = 0.0
+    var threshold : Float = -40.0
+    var running = false
+    var timer : Timer?
+    
+    
+    @IBAction func click() {
+        WKInterfaceDevice.current().play(.success)
+        
+        running = !running
+        if running {
+            button.setTitle("RESET")
+            timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(self.update), userInfo: nil, repeats: true)
+        } else {
+            button.setTitle("START")
+            timer?.invalidate()
+            timer = nil
+            time = 0.0
+            timerLabel.setText(String(0.0))
+        }
+        self.run()
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let fileManager = FileManager.default
+        let urls = fileManager.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDirectory = urls.first!
+        return documentDirectory.appendingPathComponent("recording.m4a")
+    }
+    
+    func initRecorder() {
+        let settings = [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 12000,
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+        ]
+        do {
+            try recorder = AVAudioRecorder(url: getDocumentsDirectory(), settings: settings)
+            recorder!.isMeteringEnabled = true
+            if !recorder!.prepareToRecord() {
+                print("Error: AVAudioRecorder prepareToRecord failed")
+            }
+        } catch {
+            print("Error: AVAudioRecorder creation failed")
+        }
+    }
+    
+    func start() {
+        recorder?.record()
+        recorder?.updateMeters()
+    }
+    
+    func loadRecordingUI() {
+        do {
+            try self.recordingSession.setCategory(AVAudioSessionCategoryRecord)
+            try self.recordingSession.setActive(true)
+            self.initRecorder()
+            self.start()
+        } catch {
+            self.loadFailUI()
+        }
+        
+    }
+    
+    func loadFailUI() {
+        button.setTitle("NAY")
+    }
+    
+    @objc func update() {
+        var decibels : Float = -120.0
+        if let recorder = recorder {
+            recorder.updateMeters()
+            decibels = recorder.averagePower(forChannel: 0)
+            mic.setText(String(decibels))
+        }
+        if decibels > threshold {
+            time += 0.1
+            timerLabel.setText(String(round(10*time) / 10))
+        }
+    }
+    
+    func run() {
+        recordingSession = AVAudioSession.sharedInstance()
+        
+        recordingSession.requestRecordPermission () {
+            [unowned self] allowed in
+            if allowed {
+                self.loadRecordingUI()
+            }
+            else {
+                self.loadFailUI()
+            }
+        }
+    }
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
--- espressotimer/ViewController.swift
+++ espressotimer/ViewController.swift
@@ -7,14 +7,115 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var mic: UILabel!
+    
+    var recordingSession: AVAudioSession!
+    var audioRecorder: AVAudioRecorder!
+    var recorder : AVAudioRecorder? = nil
+    
+    var time = 0.0
+    var running = false
+    var timer : Timer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.run()
+        self.click(nil)
+    }
+    
+    @IBAction func click(_ sender: Any?) {
+        running = !running
+        if running {
+            button.setTitle("RESET", for: .normal)
+            timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(self.update), userInfo: nil, repeats: true)
+        } else {
+            button.setTitle("START", for: .normal)
+            timer?.invalidate()
+            timer = nil
+        }
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let fileManager = FileManager.default
+        let urls = fileManager.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDirectory = urls.first!
+        return documentDirectory.appendingPathComponent("recording.m4a")
+    }
+    
+    func initRecorder() {
+        let settings = [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 12000,
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+        ]
+        do {
+            try recorder = AVAudioRecorder(url: getDocumentsDirectory(), settings: settings)
+            recorder!.isMeteringEnabled = true
+            if !recorder!.prepareToRecord() {
+                print("Error: AVAudioRecorder prepareToRecord failed")
+            }
+        } catch {
+            print("Error: AVAudioRecorder creation failed")
+        }
     }
 
+    func start() {
+        recorder?.record()
+        recorder?.updateMeters()
+    }
+    
+    func loadRecordingUI() {
+        button.setTitle("YEY", for: .normal)
+        do {
+            try self.recordingSession.setCategory(AVAudioSessionCategoryRecord)
+            try self.recordingSession.setActive(true)
+            self.initRecorder()
+            self.start()
+        } catch {
+            self.loadFailUI()
+        }
+        
+    }
+    
+    func loadFailUI() {
+        button.setTitle("NAY", for: .normal)
+    }
+    
+    @objc func update() {
+        var decibels : Float = -120.0
+        if let recorder = recorder {
+            recorder.updateMeters()
+            decibels = recorder.averagePower(forChannel: 0)
+            mic.text = String(decibels)
+        }
+        if decibels > -30.0 {
+            time += 0.1
+            timerLabel.text = (String(round(10*time) / 10))
+        }
+    }
+    
+    func run() {
+        recordingSession = AVAudioSession.sharedInstance()
+        
+        recordingSession.requestRecordPermission () {
+            [unowned self] allowed in
+            if allowed {
+                self.loadRecordingUI()
+            }
+            else {
+                self.loadFailUI()
+            }
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
